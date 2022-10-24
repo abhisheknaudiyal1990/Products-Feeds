ca6395d2065515418d0920f71d1bc7930ccaeaf7
Abhishek, Naudiyal <abhisheknaudiyal1990@gmail.com>
2022-10-24 14:27:36 +0530

    First Check in

---
+++ GartnerAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace GartnerAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a file option from the following list:");
+            Console.WriteLine("\tc - Capterra.yaml");
+            Console.WriteLine("\ts - Softwareadvice.json");
+            Console.Write("Your Option? ");
+            
+            switch(Console.ReadLine())
+            {
+                case "c":
+                    if (!FileParser.ParseFile("Capterra.yaml"))
+                    Console.Write("Capterra.yaml file parsing not successfull");
+                    break;
+
+                case "s":
+                    if (!FileParser.ParseFile("Softwareadvice.json"))
+                        Console.Write("Softwareadvice.json file parsing not successfull");
+                    break;
+            }
+            Console.Write("Press any key to close the console app...");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Products-Feeds
+To run this app:
+You need to install visual studio 2019 with .net core 3.1 
+Clone this repository and directly run 
+
+I've added capterra.yaml & softwareadvice.json coming from command line arguments
+
